4b183285742a7adad56a0604af54e4face6e446b
RiccardoSansone <r.sansone@gmail.com>
2023-10-11 15:07:11 +0200

    ad initial structure

--- index.php
+++ index.php
@@ -2,3 +2,32 @@
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>phpPasswordGenerator</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@300;500;700&family=PT+Sans:wght@400;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+</head>
+<body class="bg-blu">
+
+    <h1 class="color-gray text-center mt-4">Strong Password Generator</h1>
+    <h2 class="color-white text-center mt-2">Genera una password sicura</h2>
+    <h6 class="text-center mt-2">Indica in basso la lunghezza che desideri per la tua password</h6>
+    <!-- container for center all -->
+    <div class="container d-flex justify-center">
+        <!-- form for password length -->
+        <form action="" method="$_GET" class="mt-1">
+            <input type="number" name="passLength" placeholder="type your number">
+            <button type="submit">GENERA</button>
+        </form>
+        <!-- /form -->
+    </div>
+    <!-- /container -->
+
+</body>
+</html>
